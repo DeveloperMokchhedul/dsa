f950c5e76d1b1ee5ddf5cf772dd46fe0087b8284
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-04-23 10:31:03 +0600

    same functionality use loop and recursion

--- recursion.js
+++ recursion.js
@@ -2,26 +2,44 @@
 
 
 
-greet()
+// greet()
 
 
-function greet() {
-    console.log("he this is recursion practice");
-    enjoy()
-    console.log("greet end");
+// function greet() {
+//     console.log("he this is recursion practice");
+//     enjoy()
+//     console.log("greet end");
     
 
-}
+// }
 
 
 
-function enjoy(){
-    console.log("enjoy");
+// function enjoy(){
+//     console.log("enjoy");
+//     temp()
+//     console.log('Enjoy end');
+    
+// }
+
+// function temp(){
+//     console.log("temp function end")
+    
+// }
+
+
+// recursion: বড় সমস্যা কে ছোট ছোট সমস্যায় পরিনত করে সমস্যা সমাধান করার পক্রিয়াই হলো recursion
+
+for(let i=1; i<=5; i++){
+    console.log(i);
+    
     
 }
 
-function temp(){
-    console.log("enjoy");
-    
-}
+function recursion(n){
+    if (n==0)  return
+    console.log(n);
+    recursion(n-1) 
 
+}
+recursion(5)
